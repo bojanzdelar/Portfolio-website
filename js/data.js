6395b3aed9759225e5d313d438bf433f6397391e
Bojan Zdelar <bojan.zdelar@gmail.com>
2021-02-15 13:32:06 +0300

    Added data.js

---
+++ js/data.js
@@ -0,0 +1,82 @@
+const achievements = [
+    {
+        "image" : "/assets/achievements/2020-wc-k1-1000.jpg",
+        "title" : "World Cup 2020",
+        "text" : "Ninth place in K1 1000m"
+    },
+    {
+        "image" : "/assets/achievements/2019-wch-k1-1000.jpg",
+        "title" : "World Championships 2019",
+        "text" : "Eleventh place in K1 1000m"
+    },
+    {
+        "image" : "/assets/achievements/2019-ech-u23-k1-1000.jpeg",
+        "title" : "European U23 Championships 2019",
+        "text" : "Gold medal in K1 1000m"
+    },
+    {
+        "image" : "/assets/achievements/2019-ech-u23-k1-500.jpeg",
+        "title" : "European U23 Championships 2019",
+        "text" : "Silver medal in K1 500m"
+    },
+    {
+        "image" : "/assets/achievements/2018-wch-jun-k1-500.png",
+        "title" : "World Junior Championships 2018",
+        "text" : "Bronze medal in K1 500m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "European Junior Championships 2018",
+        "text" : "Silver medal in K1 500m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2017",
+        "text" : "Gold medal in K1 500m",
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2017",
+        "text" : "Gold medal in K1 200m",
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2017",
+        "text" : "Gold medal in K2 200m",
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "World Junior Championships 2017",
+        "text" : "Silver medal in K1 500m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "European Junior Championships 2017",
+        "text" : "Bronze medal in K1 1000m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "European Junior Championships 2017",
+        "text" : "Bronze medal in K1 5000m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2016",
+        "text" : "Gold medal in K1 200m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2016",
+        "text" : "Gold medal in K1 500m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "Olympic Hopes 2016",
+        "text" : "Bronze medal in K1 1000m"
+    },
+    {
+        "image" : "/assets/achievements/2018-ech-jun-k1-500.jpeg",
+        "title" : "European Junior Championships 2016",
+        "text" : "Bronze medal in K1 500m"
+    }
+]
